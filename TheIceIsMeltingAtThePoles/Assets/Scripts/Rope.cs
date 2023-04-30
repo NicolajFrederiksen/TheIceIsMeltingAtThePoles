198011235452a5f219812e4fd39e7acd79b7c15b
Nick B. Blume <72729893+nick1com@users.noreply.github.com>
2023-04-30 10:16:54 +0200

    this is my work!
    Rope and blocks to hit implimentet.

--- TheIceIsMeltingAtThePoles/Assets/Scripts/Rope.cs
+++ TheIceIsMeltingAtThePoles/Assets/Scripts/Rope.cs
@@ -4,115 +4,161 @@ using UnityEngine;
 
 public class Rope : MonoBehaviour
 {
-    public float degreesPerSecond = 10.0f;
-
-    private Vector3 LocalLengthenDirection;
+    public float degreesPerSecond;
+    public float MaxDegreesRotationAroundMC = 0.55f;
+    public float scaleSpeed, maxMovement, MaxYScale, MinScale, DestroyTimeDelay;
     private Vector3 objectAngles;
+    private bool Retract, Throwing, RopeThrown, positive = true;
+    private float currentScaleY, currentPositionY;
     MainCharacter ParentMainCharacter;
-    public bool RopeThrown;
-    public bool positive = true;
-    public float scaleSpeed, maxUpwardMovement;
-    public float maxYScale;
-    public float moveSpeed;
-    public bool collidedWithToHit, collidedWithY;
 
     // Start is called before the first frame update
     void Start()
     {
         objectAngles = new Vector3(0, 0, 1);
-        LocalLengthenDirection = new Vector3(0, 1, 0);
         ParentMainCharacter = GetComponentInParent<MainCharacter>();
-       
+        currentScaleY = transform.localScale.y;
+        currentPositionY = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            RopeThrown = true;
-        }
-        if (Input.GetMouseButtonDown(1))
-        {
-            RopeThrown = false;
-        }
         if (RopeThrown)
         {
             ThrowRope();
         }
-        if (!RopeThrown)
+        if (!Throwing && !Retract)
         {
             RopeRotateAroundMC();
         }
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Throwing = true;
+            Retract = false;
+            RopeThrown = true;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Throwing = false;
+            Retract = true;
+            RopeThrown = true;
+        }
+
     }
 
     public void RopeRotateAroundMC()
     {
-        if (!RopeThrown)
+        if (transform.rotation.z >= MaxDegreesRotationAroundMC)
         {
-            if (transform.rotation.z >= 0.55)
+            if (positive)
             {
-                if (positive)
-                {
-                    degreesPerSecond *= -1;
-                    positive = false;
-                }
+                degreesPerSecond *= -1;
+                positive = false;
             }
-            else if (transform.rotation.z <= -0.55)
-            {
-                if (!positive)
-                {
-                    degreesPerSecond *= -1;
-                    positive = true;
-                }
-
-            }
-            transform.RotateAround(transform.parent.position, objectAngles, degreesPerSecond * Time.deltaTime);
         }
+        else if (transform.rotation.z <= -MaxDegreesRotationAroundMC)
+        {
+            if (!positive)
+            {
+                degreesPerSecond *= -1;
+                positive = true;
+            }
+
+        }
+        transform.RotateAround(transform.parent.position, objectAngles, degreesPerSecond * Time.deltaTime);
+
     }
 
-    public void ThrowRope(){
+    public void ThrowRope()
+    {
+        currentScaleY = transform.localScale.y;
+        currentPositionY = transform.position.y;
+        if (transform.localScale.y >= MaxYScale || Input.GetKeyDown(KeyCode.Return))
+        {
+            Retract = true;
+            Throwing = false;
 
-        float currentScaleY = transform.localScale.y;
-        float currentPositionY = transform.position.y;
 
-        // Increase the scale of the object in the Y direction
-        float newScaleY = currentScaleY + (scaleSpeed * Time.deltaTime);
+        }
+        if (transform.localScale.y <= MinScale)
+        {
+            Retract = false;
+        }
 
-        // Clamp the Y scale to the maximum value and only allow positive scale values
-        newScaleY = Mathf.Clamp(newScaleY, 0f, maxYScale);
+        if (Retract)
+        {
 
-        // Calculate the difference between the new and current Y scale
-        float scaleDelta = newScaleY - currentScaleY;
+            // Decrease the scale of the object in the Y direction
+            float newScaleYD = currentScaleY - (scaleSpeed * Time.deltaTime);
 
-        // Move the object upward by a distance proportional to the scale delta
-        float upwardMovement = Mathf.Lerp(0f, maxUpwardMovement, scaleDelta / maxYScale);
-        transform.position += transform.up * upwardMovement;
+            // Clamp the Y scale to the minimum value and only allow positive scale values
+            newScaleYD = Mathf.Clamp(newScaleYD, MinScale, MaxYScale);
 
-        // Set the new local scale of the object, only affecting the Y component
-        transform.localScale += new Vector3(0f, scaleDelta, 0f);
+            // Calculate the difference between the new and current Y scale
+            float scaleDelta = newScaleYD - currentScaleY;
+            // Move the object downward by a distance proportional to the scale delta
+            float downwardMovement = Mathf.Lerp(0f, maxMovement, -scaleDelta / MaxYScale);
+            this.transform.position -= transform.up * downwardMovement;
 
-        // Clamp the object's upward movement to the maximum value
-        float TargetPositionY = transform.position.y;
-        TargetPositionY = Mathf.Clamp(TargetPositionY, currentPositionY, currentPositionY + maxUpwardMovement);
-        transform.position = new Vector3(transform.position.x, TargetPositionY, transform.position.z);
+            // Set the new local scale of the object, only affecting the Y component   
+            transform.localScale += new Vector3(0f, scaleDelta, 0f);
 
+            // Clamp the object's downward movement to the maximum value
+            float TargetPositionYD = transform.position.y;
+            TargetPositionYD = Mathf.Clamp(TargetPositionYD, currentPositionY - maxMovement, currentPositionY);
+            transform.position = new Vector3(transform.position.x, TargetPositionYD, transform.position.z);
+        }
+        if (Throwing)
+        {
+            {
+                // Increase the scale of the object in the Y direction
+                float newScaleY = currentScaleY + (scaleSpeed * Time.deltaTime);
+
+                // Clamp the Y scale to the maximum value and only allow positive scale values
+                newScaleY = Mathf.Clamp(newScaleY, MinScale, MaxYScale);
+
+                // Calculate the difference between the new and current Y scale
+                float scaleDelta = newScaleY - currentScaleY;
+
+                // Move the object upward by a distance proportional to the scale delta
+                float upwardMovement = Mathf.Lerp(0f, maxMovement, scaleDelta / MaxYScale);
+                this.transform.position += transform.up * upwardMovement;
+
+                // Set the new local scale of the object, only affecting the Y component
+                transform.localScale += new Vector3(0f, scaleDelta, 0f);
+
+                // Clamp the object's upward movement to the maximum value
+                float TargetPositionY = transform.position.y;
+                TargetPositionY = Mathf.Clamp(TargetPositionY, currentPositionY, currentPositionY + maxMovement);
+                transform.position = new Vector3(transform.position.x, TargetPositionY, transform.position.z);
+            }
+        }
+        if (!Retract && !Throwing)
+        {
+            RopeThrown = false;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+
         ContactPoint contact = collision.contacts[0];
         // Check the tag of the colliding object
+
         if (collision.gameObject.CompareTag("ToHit"))
         {
             NoRigid();
-            collidedWithToHit = true;
+            collision.gameObject.GetComponent<ToHitObject>().destroyIt(DestroyTimeDelay);
             Vector3 TargetPosition = collision.gameObject.transform.position;
             ParentMainCharacter.MovingToObject(TargetPosition);
+
         }
         else if (collision.gameObject.CompareTag("ObjectY"))
         {
-            collidedWithY = true;
+
         }
     }
     public void NoRigid()

76aec51a134822851f951675b313bc4e5c6ae793
Nicolaj Frederiksen <72729883+NicolajFrederiksen@users.noreply.github.com>
2023-04-30 12:02:47 +0200

    animator

--- TheIceIsMeltingAtThePoles/Assets/Scripts/MenuHandler.cs
+++ TheIceIsMeltingAtThePoles/Assets/Scripts/MenuHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartGameScript : MonoBehaviour
+public class MenuHandler : MonoBehaviour
 {
     public string GameScene;
     
